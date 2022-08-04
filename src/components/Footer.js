aaa95a1e1df592d3ac6cc38a1f2946152354ed67
Ihor Radetskyi <mrsettle1@gmail.com>
2022-08-04 10:24:17 +0300

    sun

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Sun from './Sun'
 
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy; 2021</p>
+      <p>blind0ne &copy; 2022</p>
       <Link to='/about'>About</Link>
+      <Sun/>
     </footer>
   )
 }
---
+++ src/components/Sun.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sun = () => {
+  return (
+    <div className='centered'>
+        <div className="sun">
+            Blind0ne
+        </div>
+    </div>
+  )
+}
+
+export default Sun
